b9cc234a4a9caef45d458c02509e5265786fa190
Clement Baudonnel <33254234+Skyrize@users.noreply.github.com>
2019-10-14 15:13:55 +0200

    [Feature] bumper
    Adding bumper done

---
+++ Assets/Scripts/BumpComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumpComponent : MonoBehaviour
+{
+    [Header("Attributes")]
+    public float ejectionForce = 10;
+
+    // private void OnTriggerEnter(Collider other) {
+        
+    // }
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag.CompareTo("Player") == 0) {
+        }
+        other.gameObject.GetComponent<Rigidbody>().AddForce(transform.up * ejectionForce);
+    }
+}
