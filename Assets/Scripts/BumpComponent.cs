ed8a9ea41b452c8f91b71b45fe982415df637a77
Loïc Pauleau <loic.pauleau@gmail.com>
2019-10-18 16:20:45 +0200

    Correction BumpScene
    Ajout des particules sur le bump

--- Assets/Scripts/BumpComponent.cs
+++ Assets/Scripts/BumpComponent.cs
@@ -6,14 +6,14 @@ public class BumpComponent : MonoBehaviour
 {
     [Header("Attributes")]
     public float ejectionForce = 1;
+    public ParticleSystem particlebump;
 
-    // private void OnTriggerEnter(Collider other) {
-        
-    // }
+    
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag.CompareTo("Player") == 0) {
             Debug.Log("Boing");
             other.gameObject.GetComponent<MovementComponent>().Propulse(transform.up * ejectionForce);
+            particlebump.Play();
         }
     }
 }
