a6f927a0701c40a7f6f0d1f454af395ad65fd517
Loïc Pauleau <loic.pauleau@gmail.com>
2019-10-18 14:00:49 +0200

    Import New FBX player

--- Assets/Scripts/BumpComponent.cs
+++ Assets/Scripts/BumpComponent.cs
@@ -11,7 +11,8 @@ public class BumpComponent : MonoBehaviour
         
     // }
     private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag.CompareTo("Player") == 0) {
+        if (other.gameObject.tag.CompareTo("Player") == 0)
+        {
             Debug.Log("Boing");
             other.gameObject.GetComponent<MovementComponent>().AddForce(transform.up * ejectionForce);
         }
