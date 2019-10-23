eb810d85e52ad2d8a5e0517ad1a0fa3d115ae458
antoine falais <antoine.falais@epitech.eu>
2019-10-23 21:24:22 +0200

    Speedpowerup (random spwan)

--- Assets/Scripts/Powersup/Speed/SpeedPowerupController.cs
+++ Assets/Scripts/Powersup/Speed/SpeedPowerupController.cs
@@ -2,18 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpeedPowerupController : MonoBehaviour
 {
     public GameObject powerupPrefab;
-    public GameObject Origin;
-
+    public SpeedPowerupController Origin;
     public List<SpeedPowerup> powerups;
     public int duration;
-
-
     public Dictionary<SpeedPowerup, float> activePowerups = new Dictionary<SpeedPowerup, float>();
-
     private List<SpeedPowerup> keys = new List<SpeedPowerup>();
 
 
@@ -23,7 +20,7 @@ public class SpeedPowerupController : MonoBehaviour
         HandeActivePowerups();
         if (Input.GetKeyDown(KeyCode.T))
         {
-            SpawnPowerup(powerups[0], new Vector3(-1.925f, 2.646f, 1.75f)) ;
+            SpawnPowerup() ;
         }
     }
 
@@ -66,17 +63,34 @@ public class SpeedPowerupController : MonoBehaviour
         keys = new List<SpeedPowerup>(activePowerups.Keys);
     }
 
-    public GameObject SpawnPowerup(SpeedPowerup powerup, Vector3 position)
+    public void SpawnPowerup()
     {
-        try
+        Quaternion rotation = Quaternion.Euler(0, 0, 0);
+        Vector3 position = getRandomPosition();
+        Instantiate(Origin, position, rotation);
+    }
+
+    private Vector3 getRandomPosition()
+    {
+        float minX = -4f;
+        float maxX = 4.60f;
+        float badMinX = -0.601f;
+        float badMaxX = 1.32f;
+        float minY = 0f;
+        float maxY = 4f;
+        float badMaxY = 3.26f;
+        float badMinY = 2;
+        Vector3 vector = new Vector3();
+        vector.z = 1.75f;
+        do
         {
-            Quaternion rotation = Quaternion.Euler(0,0,0);
-            GameObject powerupGameObject = Instantiate(Origin, position, rotation);
-            return powerupGameObject;
-        }
-        catch (Exception)
+            vector.x = Random.Range(minX, maxX);
+        } while (vector.x >= badMinX && vector.x <= badMaxX);
+        do
         {
-            return null;
-        }
+            vector.y = Random.Range(minY, maxY);
+        } while (vector.y >= badMinY && vector.y <= badMaxY);
+
+        return vector;
     }
 }
