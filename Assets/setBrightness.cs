10d3af8bf53fa858714d6a0113e7cec64bfdf4f9
Camille Villa <camille.villa@epitech.eu>
2019-10-29 14:27:21 +0100

    add begin of pannels (options / start / end)

---
+++ Assets/setBrightness.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class setBrightness : MonoBehaviour
+{
+    public Slider mainSlider;
+    public float GammaCorrection;
+   
+    void Update() {
+        if (GameObject.FindGameObjectWithTag ("brightness")) {
+            mainSlider = (Slider) FindObjectOfType(typeof (Slider));
+        }
+        GammaCorrection = mainSlider.value;
+        Debug.Log(GammaCorrection);
+        RenderSettings.ambientLight = new Color(mainSlider.value, mainSlider.value, mainSlider.value, 1.0f);
+       
+    }
+   
+}
