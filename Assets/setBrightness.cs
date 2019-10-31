f61c3b10ffb5a7d0e6d138eb00f8006a68f06752
Loïc Pauleau <loic.pauleau@gmail.com>
2019-10-31 13:49:33 +0100

    Brightness settings + particles EndJump

--- Assets/setBrightness.cs
+++ Assets/setBrightness.cs
@@ -1,21 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class setBrightness : MonoBehaviour
 {
+    float RGBvalue = 0.5f;    
     public Slider mainSlider;
-    public float GammaCorrection;
-   
-    void Update() {
-        if (GameObject.FindGameObjectWithTag ("brightness")) {
-            mainSlider = (Slider) FindObjectOfType(typeof (Slider));
-        }
-        GammaCorrection = mainSlider.value;
-        Debug.Log(GammaCorrection);
-        RenderSettings.ambientLight = new Color(mainSlider.value, mainSlider.value, mainSlider.value, 1.0f);
-       
+
+    public void SettingsBright()
+    {
+        RGBvalue = mainSlider.value;
+        RenderSettings.ambientLight = new Color(RGBvalue, RGBvalue, RGBvalue, 1);
     }
-   
 }
