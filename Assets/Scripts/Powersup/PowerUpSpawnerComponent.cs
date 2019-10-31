13b5184657fd2dcf5321ba1b404ef3481547dbbb
Clement Baudonnel <33254234+Skyrize@users.noreply.github.com>
2019-10-31 11:58:39 +0100

    Feature : adding only one powerUp at a time

--- Assets/Scripts/Powersup/PowerUpSpawnerComponent.cs
+++ Assets/Scripts/Powersup/PowerUpSpawnerComponent.cs
@@ -8,7 +8,14 @@ public class PowerUpSpawnerComponent : MonoBehaviour
     public float maximumeTime = 3;
     public GameObject[] powerUps;
     public SpawnPointComponent[] points;
+    [SerializeField]
     private float timer = 0;
+
+    /*
+        Ce code il est pas beau faut pas le regarder dans les yeux franchement c'est pas bien fait j'avais la flemme hihi
+    */
+    [SerializeField]
+    private bool theOnlyOneIsHere;
 
     private void Awake()
     {
@@ -39,23 +46,31 @@ public class PowerUpSpawnerComponent : MonoBehaviour
         return false;
     }
 
+    public void TheOnlyOneHasBeenTaken()
+    {
+        theOnlyOneIsHere = false;
+    }
+
     void SpawnRandomPowerUp()
     {
         int index = Random.Range(0, powerUps.Length);
         SpawnPointComponent point = GetRandomPoint();
         Instantiate(powerUps[index], point.transform.position, point.transform.rotation);
         point.isHolding = true;
+        theOnlyOneIsHere = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer > 0) {
-            timer -= Time.deltaTime;
-        } else {
-            timer = Random.Range(minimumTime, maximumeTime);
-            if (CanSpawn())
-                SpawnRandomPowerUp();
+        if (!theOnlyOneIsHere) {
+            if (timer > 0) {
+                timer -= Time.deltaTime;
+            } else {
+                timer = Random.Range(minimumTime, maximumeTime);
+                if (CanSpawn())
+                    SpawnRandomPowerUp();
+            }
         }
     }
 }
