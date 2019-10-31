22676963071dc621a791912df820a85dc24d7ff0
Clement Baudonnel <33254234+Skyrize@users.noreply.github.com>
2019-10-31 13:21:26 +0100

    Feature : adding assets for powerUps

---
+++ Assets/Scripts/PowerUps/Speed/SpeedPowerup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class SpeedPowerup
+{
+    [SerializeField]
+    public string name;
+
+    public UnityEvent startAction;
+    public UnityEvent endAction;
+
+    public void End()
+    {
+        if (endAction != null)
+            endAction.Invoke();
+    }
+
+    public void Start()
+    {
+        if (startAction != null)
+            startAction.Invoke();
+    }
+
+
+}
