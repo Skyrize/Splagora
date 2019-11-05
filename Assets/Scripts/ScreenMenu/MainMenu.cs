b9f60f4437fd3a0f3eda7ac1c2f79daecfeee3a0
Loïc Pauleau <loic.pauleau@gmail.com>
2019-11-05 15:05:14 +0100

    UI + Spray Particules

---
+++ Assets/Scripts/ScreenMenu/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void startPlaying()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void BacktoMenu()
+    {
+        SceneManager.LoadScene("ScreenMenu", 0);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/ScreenMenu/SetContraste.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
+
+public class SetContraste : MonoBehaviour
+{        
+    public Slider contrastSlider;
+    ColorGrading colorGrading;
+
+    void Start()
+    {
+        PostProcessVolume myProcess = Camera.main.GetComponent<PostProcessVolume>();
+        myProcess.profile.TryGetSettings(out colorGrading);
+    }      
+
+    public void ModifyContrast(float ContrastValue)
+    {
+        ContrastValue = contrastSlider.value;
+        colorGrading.contrast.value = ContrastValue * 100;       
+    }
+}
