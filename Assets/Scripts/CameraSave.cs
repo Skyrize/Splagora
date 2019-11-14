ecc36cd76507c86682be71dfd38439eb3b745b42
Loïc Pauleau <loic.pauleau@gmail.com>
2019-11-14 13:09:30 +0100

    Add Camera.cs

---
+++ Assets/Scripts/CameraSave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSave : MonoBehaviour
+{
+    /*
+    public static Camera CameraInstance = null;
+
+    private void Awake()
+    {
+        if (CameraInstance == null)
+        {
+            CameraInstance = this;
+        }
+        else if (CameraInstance != this)
+        {
+            Destroy(CameraInstance);
+        }
+        DontDestroyOnLoad(CameraInstance);
+    }   */
+    void Update()
+    {
+        
+    }
+}
